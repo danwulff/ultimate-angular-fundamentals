8626e1f7fa470dd0133b5345d454abc13c27042e
Daniel Wulff <daniel.wulff@dealerpeak.com>
2018-01-09 12:06:01 -0800

    passing data into components with @input

--- app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Passenger } from '../../models/passenger.interface';
 
 @Component({
   selector: 'passenger-count',
   template: `
+    <h3>Airline Passengers!</h3>
     <div>
-      Count component
+      Total checked in: {{ checkedInCount() }}/{{ items.length }}
     </div>
   `
 })
 export class PassengerCountComponent {
-  constructor(){ }
+  @Input()
+  items: Passenger[];
+
+  checkedInCount(): number {
+    if (!this.items) return;
+    return this.items.filter((passenger: Passenger) => passenger.checkedIn).length;
+  }
 }
