0a6f9982d575f0e2e7851528c9bcc581f10e339d
Daniel Wulff <danielwulff.ee@gmail.com>
2018-05-19 13:16:13 -0700

    Forms container component

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,23 +1,29 @@
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
 
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+import "rxjs/add/observable/throw";
 
-import { Passenger } from './models/passenger.interface';
+import { Passenger } from "./models/passenger.interface";
 
-const PASSENGER_API: string = '/api/passengers';
+const PASSENGER_API: string = "/api/passengers";
 
 @Injectable()
 export class PassengerDashboardService {
-
   constructor(private http: Http) {}
 
   getPassengers(): Observable<Passenger[]> {
     return this.http
       .get(PASSENGER_API)
+      .map((response: Response) => response.json())
+      .catch((error: any) => Observable.throw(error.json));
+  }
+
+  getPassenger(id: number): Observable<Passenger> {
+    return this.http
+      .get(`${PASSENGER_API}/${id}`)
       .map((response: Response) => response.json())
       .catch((error: any) => Observable.throw(error.json));
   }
@@ -35,5 +41,4 @@ export class PassengerDashboardService {
       .map((response: Response) => response.json())
       .catch((error: any) => Observable.throw(error.json));
   }
-
 }
