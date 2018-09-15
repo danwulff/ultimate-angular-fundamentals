f91fe0effacd7e7ea67df82818ddda18176f3b10
Daniel Wulff <danielwulff.ee@gmail.com>
2018-09-15 14:13:13 -0700

    <select> option rendering, and ngValue

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 
+import { Baggage } from '../../models/baggage.interface';
 import { Passenger } from '../../models/passenger.interface';
 
 @Component({
@@ -43,17 +44,57 @@ import { Passenger } from '../../models/passenger.interface';
           [ngModel]="detail?.checkInDate">
       </div>
 
+      <div>
+        Luggage:
+        <select
+          name="baggage"
+          [ngModel]="detail?.baggage">
+          <option
+            *ngFor="let item of baggage"
+            [value]="item.key"
+            [selected]="item.key === detail?.baggage">
+            {{ item.value }}
+          </option>
+        </select>
+        <!-- another style of the above with ngValue
+        <select
+          name="baggage"
+          [ngModel]="detail?.baggage">
+          <option
+            *ngFor="let item of baggage"
+            [ngValue]="item.key">
+            {{ item.value }}
+          </option>
+        </select> -->
+      </div>
+
       {{ form.value | json }}
     </form>
   `
 })
 export class PassengerFormComponent {
+
   @Input()
   detail: Passenger;
+
+  baggage: Baggage[] = [{
+    key: 'none',
+    value: 'No baggage'
+  }, {
+    key: 'hand-only',
+    value: 'Hand baggage'
+  }, {
+    key: 'hold-only',
+    value: 'Hold baggage'
+  }, {
+    key: 'hand-hold',
+    value: 'Hand and hold baggage'
+  }];
 
   toggleCheckIn(checkedIn: boolean) {
     if (checkedIn) {
       this.detail.checkInDate = Date.now();  // ms
     }
   }
+
 }
